1c3a47d388a521ad0c834649b881f84021e5f96b
Danilo Korol <777danilkorol777@gmail.com>
2023-01-30 23:11:50 +0200

    done 3 tasks

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const formRefs = {
+    email: document.querySelector('input'),
+    message: document.querySelector('textarea'),
+    form: document.querySelector('.feedback-form'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+const userData = {};
+
+updateUserData();
+
+formRefs.form.addEventListener('input', onFormInput);
+formRefs.form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(event) {
+    const key = event.target.name;
+    const value = event.target.value;
+    userData[key] = value;
+    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(userData));
+};
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    console.log(userData);
+
+    event.currentTarget.reset();
+
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function updateUserData() {
+    const data = localStorage.getItem(STORAGE_KEY);
+    const parseData = JSON.parse(data);
+
+    const email = parseData.email;
+    const message = parseData.message;
+    
+    if (email) {
+        formRefs.email.value = email;
+    }
+    if (message) {
+        formRefs.message.value = message;
+    }
+};
